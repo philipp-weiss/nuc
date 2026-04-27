45a94797b85026f9a01485e2ce5a67ddb3bfeba6
philipp-weiss <philipp.weiss@web.de>
2026-04-27 21:10:22 +0200

    chore: upgrade nixpkgs to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS NUC Konfiguration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
